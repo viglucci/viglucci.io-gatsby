743b1d9355308d6030f59197ce9df63cf4460f41
Kevin Viglucci <kviglucci@gmail.com>
2020-05-10 04:34:25 -0500

    update header of articles section on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,13 +58,15 @@ const PostList = ({
         <div className="bg-white h-1/3 sm:h-2/3"></div>
       </div>
       <div className="relative max-w-7xl mx-auto">
-        <div className="text-center">
-          <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
-            Blog Posts
+        <div>
+          <h2 class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10 ">
+            Articles
           </h2>
-          <p className="mt-3 max-w-2xl mx-auto text-xl leading-7 text-gray-500 sm:mt-4">
-            I enjoy writing about Software Architecture, Javascript, APIs, and various other topics. You can find my latest articles below.
-          </p>
+          <div class="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
+            <p class="text-xl leading-7 text-gray-500">
+              I enjoy writing about Software Architecture, Javascript, APIs, and various other topics. You can find my latest articles below.
+            </p>
+          </div>
         </div>
         <div className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
           {posts.map((post) => <PostCard {...post} key={post.fields.slug} />)}
