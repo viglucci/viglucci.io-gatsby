e84dc508cfc01510f561c978a8777d99682b1e26
Kevin Viglucci <kviglucci@gmail.com>
2020-05-10 16:49:25 -0500

    fix class attribute

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,11 +59,11 @@ const PostList = ({
       </div>
       <div className="relative max-w-7xl mx-auto">
         <div>
-          <h2 class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10 ">
+          <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10 ">
             Articles
           </h2>
-          <div class="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
-            <p class="text-xl leading-7 text-gray-500">
+          <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
+            <p className="text-xl leading-7 text-gray-500">
               Kevin enjoys writing about Software Architecture, Javascript, APIs, and various other topics. You can find his latest articles below.
             </p>
           </div>
