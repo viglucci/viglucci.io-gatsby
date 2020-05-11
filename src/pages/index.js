0281048559d75444758f4441d17fe9d95e9854d2
Kevin Viglucci <kviglucci@gmail.com>
2020-05-10 23:39:03 -0500

    fix padding on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,34 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import indexHeader from "../../content/assets/index-header.jpg";
 
+const Header = () => {
+  return (
+    <div className="relative bg-gray-800 overflow-hidden">
+      <div className="max-w-screen-xl mx-auto ">
+        <div className="relative z-10 pb-8 bg-gray-800 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-gray-800 transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+          <Nav />
+          <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="text-left">
+              <h2 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl sm:leading-none md:text-6xl">
+                Kevin Viglucci
+              </h2>
+              <div className="mt-3 max-w-3xl text-lg leading-6 text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl lg:mx-0">
+                Kevin is a software engineer from Austin, Texas. Kevin works at Blizzard Entertainment where he builds tools, APIs, and experiences that support World of Warcraft on the web.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={indexHeader} alt="" />
+      </div>
+    </div>
+  );
+}
+
 const PostCard = (props) => {
   const {
     fields,
@@ -53,13 +81,10 @@ const PostList = ({
   posts
 }) => {
   return (
-    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-      <div className="absolute inset-0">
-        <div className="bg-white h-1/3 sm:h-2/3"></div>
-      </div>
+    <div className="relative pt-16 pb-20 lg:pt-24 lg:pb-28">
       <div className="relative max-w-7xl mx-auto">
         <div>
-          <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10 ">
+          <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-700 sm:text-4xl sm:leading-10 ">
             Articles
           </h2>
           <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
@@ -76,34 +101,6 @@ const PostList = ({
   );
 };
 
-const Header = () => {
-  return (
-    <div className="relative bg-gray-800 overflow-hidden">
-      <div className="max-w-screen-xl mx-auto ">
-        <div className="relative z-10 pb-8 bg-gray-800 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-gray-800 transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none">
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
-          <Nav />
-          <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div className="text-left">
-              <h2 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl sm:leading-none md:text-6xl">
-                Kevin Viglucci
-              </h2>
-              <div className="mt-3 max-w-3xl text-lg leading-6 text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl lg:mx-0">
-                Kevin is a software engineer from Austin, Texas. Kevin works at Blizzard Entertainment where he builds tools, APIs, and experiences that support World of Warcraft on the web.
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={indexHeader} alt="" />
-      </div>
-    </div>
-  );
-}
-
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props;
@@ -113,8 +110,9 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={""}>
         <SEO title="Kevin Viglucci" />
         <Header />
-        {/* <Bio /> */}
-        <PostList posts={posts} />
+        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <PostList posts={posts} />
+        </div>
       </Layout>
     );
   }
