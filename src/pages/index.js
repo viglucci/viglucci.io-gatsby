451bfee0c86ecf7c419e4c2b63ef368829570652
Kevin Viglucci <kviglucci@gmail.com>
2020-05-11 00:13:43 -0500

    update copy on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,9 +81,9 @@ const PostList = ({
           <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-700 sm:text-4xl sm:leading-10 ">
             Articles
           </h2>
-          <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
+          <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-1 lg:gap-5 lg:items-center">
             <p className="text-xl leading-7 text-gray-500">
-              Kevin enjoys writing about Software Architecture, Javascript, APIs, and various other topics. You can find his latest articles below.
+              Kevin enjoys writing about software architecture, web APIs, Javascript, and various other topics.
             </p>
           </div>
         </div>
